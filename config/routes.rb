a1f936f5be1b5a61a005e4dad72c78b7942f955b
ashu02-git <shuhei.abe1107@gmail.com>
2020-11-20 07:28:22 +0000

    [Add] Newbook投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :edit, :update, :destroy]
 end
