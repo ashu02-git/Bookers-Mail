512857ede6441e3082c7a702fb11fc283bd058ce
ashu02-git <shuhei.abe1107@gmail.com>
2020-11-21 02:05:11 +0000

    [Add] User一覧追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 end
