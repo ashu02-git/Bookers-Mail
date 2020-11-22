0311727ee97ef54b49ff72ce1adf398b33808c55
ashu02-git <shuhei.abe1107@gmail.com>
2020-11-22 01:55:26 +0000

    [Add] 各ユーザの投稿一覧追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
+
   end
-  
+
   def show
     @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
@@ -15,7 +17,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)  
+    redirect_to user_path(@user.id)
   end
 
   private
